ac792d05548c20d061f18a70504eb56b28943f40
santiago1708 <vela123tique@gmail.com>
2025-11-20 21:41:56 -0500

    optimizacion de axios

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 
 export default function AuthLayout() {
@@ -12,6 +13,7 @@ export default function AuthLayout() {
                     </div>
                 </div>
             </div>
+            <Toaster position='top-right'/>
         </>
     )
 }
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useForm } from 'react-hook-form'
-import axios, { isAxiosError } from "axios";
+import { isAxiosError } from "axios";
+import { toast } from 'sonner'
 import ErrorMessage from "../components/ErrorMessage";
 import type { RegisterForm } from "../types";
+import api from "../config/axios";
 
 export default function RegisterView() {
 
@@ -14,18 +16,20 @@ export default function RegisterView() {
         password_confirmation: ''
     }
 
-    const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterForm>({ defaultValues: initialValues });
+    const { register, watch, handleSubmit, reset, formState: { errors } } = useForm<RegisterForm>({ defaultValues: initialValues });
 
     const password = watch('password');
-    
+
 
     const handleRegister = async (formData: RegisterForm) => {
         try {
-            const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/register`, formData)
-            console.log(data);
+            const { data } = await api.post(`/auth/register`, formData)
+            toast.success(data)
+
+            reset();
         } catch (error) {
-            if(isAxiosError(error) && error.response) {
-                console.log(error.response.data.error);
+            if (isAxiosError(error) && error.response) {
+                toast.error(error.response.data.error);
             }
         }
     }
