14946a6a992f995368951d283d28558e3155d3ac
santiago1708 <vela123tique@gmail.com>
2025-11-21 21:00:16 -0500

    implemento JWT

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -18,7 +18,7 @@ export default function LoginView() {
     const handleLogin = async (formData : LoginForm) => {
         try {
             const { data } = await api.post(`/auth/login`, formData)
-            toast.success(data)
+            localStorage.setItem('AuthToken', data)
             
         } catch (error) {
             if (isAxiosError(error) && error.response) {
