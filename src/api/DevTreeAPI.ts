42c2ea3a2c89dfc748c826b78d5d1d8b20a9fb19
santiago1708 <vela123tique@gmail.com>
2025-12-10 22:22:54 -0500

    Actualizacion: React Query

---
+++ src/api/DevTreeAPI.ts
@@ -0,0 +1,13 @@
+import { isAxiosError } from "axios";
+import api from "../config/axios";
+
+export async function getUser() {
+    try {
+        const { data } = await api('/user')
+        console.log(data)
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
