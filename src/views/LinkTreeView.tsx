2b0f6121790319a911df30adb6d116a8a8e9366f
santiago1708 <vela123tique@gmail.com>
2025-12-18 23:16:55 -0500

    se actualizo funciones para hacer uso de un DragNDrop

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -11,7 +11,7 @@ export default function LinkTreeView() {
 
     const [devTreeLinks, setDevTreeLinks] = useState(social)
     const queryClient = useQueryClient();
-    const user : User = queryClient.getQueryData(['user'])!
+    const user: User = queryClient.getQueryData(['user'])!
 
     const { mutate } = useMutation({
         mutationFn: updateProfile,
@@ -24,29 +24,25 @@ export default function LinkTreeView() {
     })
 
     useEffect(() => {
-        const updatedData = devTreeLinks.map( item => {
-            const userLink = JSON.parse(user.links).find( (link : SocialNetwork) => link.name === item.name)
-            if(userLink) {
-                return {...item, url: userLink.url, enabled: userLink.enabled}
+        const updatedData = devTreeLinks.map(item => {
+            const userLink = JSON.parse(user.links).find((link: SocialNetwork) => link.name === item.name)
+            if (userLink) {
+                return { ...item, url: userLink.url, enabled: userLink.enabled }
             }
             return item
         })
         setDevTreeLinks(updatedData);
-    }, []) 
+    }, [])
+
+
 
     const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? { ...link, url: e.target.value } : link)
         setDevTreeLinks(updatedLinks);
 
-        queryClient.setQueryData(['user'], (prevData : User) => {
-            return {
-                ...prevData,
-                links: JSON.stringify(updatedLinks)            
-            }
-        })
     }
-
-
+    //Obtiene los links actuales de la base de datos asi esten vacios
+    const links: SocialNetwork[] = JSON.parse(user.links)
 
     const handleEnableLink = (socialNetwork: string) => {
         const updatedLinks = devTreeLinks.map(link => {
@@ -60,13 +56,70 @@ export default function LinkTreeView() {
             }
             return link
         })
-        
+
         setDevTreeLinks(updatedLinks);
 
-        queryClient.setQueryData(['user'], (prevData : User) => {
+        //arreglo auxiliar para almacenar los link habilitados
+        let updateItem: SocialNetwork[] = []
+
+        //busca el link hablitado
+        const selectSocialNetwork = updatedLinks.find(link => link.name === socialNetwork)
+
+        /** En esta condicional primero: 
+         * 1. Verifica si el link seleccionado esta habilitado
+         *      a. Si es asi, asigna un id basado en la cantidad de links habilitados actualmente
+         *      b. Luego verifica si el link ya existe en la lista de links del usuario
+         *          i. Si existe, actualiza su estado a habilitado y asigna el id
+         *          ii. Si no existe, crea un nuevo objeto link con el id asignado y lo agrega a la lista
+         * 2. Si el link seleccionado no esta habilitado
+         */
+        if (selectSocialNetwork?.enabled) {
+            const id = links.filter(link => link.id).length + 1
+            if (links.some(link => link.name === socialNetwork)) {
+                updateItem = links.map(link => {
+                    if (link.name === socialNetwork) {
+                        return {
+                            ...link,
+                            enabled: true,
+                            id
+                        }
+                    } else {
+                        return link
+                    }
+                })
+            } else {
+                const newItem = {
+                    ...selectSocialNetwork,
+                    id
+                }
+                updateItem = [...links, newItem]
+            }
+        } else {
+            //
+            const indexToUpdate = links.findIndex(link => link.name === socialNetwork)
+            updateItem = links.map(link => {
+                if (link.name === socialNetwork) {
+                    return {
+                        ...link,
+                        id: 0,
+                        enabled: false
+                    }
+                } else if (link.id > indexToUpdate) {
+                    return {
+                        ...link,
+                        id: link.id - 1
+                    }
+                } else {
+                    return link
+                }
+            })
+        }
+
+        //almacena en la base de datos los links unicamente habilitados
+        queryClient.setQueryData(['user'], (prevData: User) => {
             return {
                 ...prevData,
-                links: JSON.stringify(updatedLinks)            
+                links: JSON.stringify(updateItem)
             }
         })
     }
